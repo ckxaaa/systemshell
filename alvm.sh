1107df0816f90cad5a27fd799b4d92c980e687af
ckxaaa <39125565+ckxaaa@users.noreply.github.com>
2019-12-20 16:53:48 +0800

    Add files via upload

---
+++ alvm.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+read -p "创建lVM输入1，删除输入2:" number
+if [ $number -eq 1 ];then
+array_str=()
+read -p "输入你要加入几块磁盘" num
+for((i=0;i<$num;i++));
+do
+    read -p "请输入你想要个格式化的磁盘"  array_str[i]
+done
+echo "正在创建叫做VolGroup的逻辑卷组"
+pvcreate /dev/${array_str[0]}
+vgcreate VolGroup /dev/${array_str[0]}
+for((i=1;i<$num;i++));
+do
+pvcreate /dev/${array_str[i]}
+vgextend VolGroup /dev/${array_str[i]}
+done
+Free_PE=`vgdisplay | grep "Free" | awk '{print $5}'`
+echo "正在创建名为lv01的卷"
+lvcreate -l +$Free_PE -n lv01 VolGroup
+LV_Path=`lvdisplay | grep "LV Path" | awk '{print $3}'`
+echo "请把 $LV_Path 格式化挂载使用,谢谢！！"
+else 
+	
+  	lvremove /dev/VolGroup/lv01
+	vgremove VolGroup00
+	m=(`pvscan | grep "PV" | awk '{print $2}'`)
+	ms=${#m[@]}
+	for ((i=0;i<=$ms;i++));
+	do
+	pvremove ${m[i]} 2> /dev/null
+	done
+	
+fi
